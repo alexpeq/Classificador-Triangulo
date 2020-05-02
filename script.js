f33b778cdfba8bcc4dbb06ca7f22f9340d443583
Alex Dias <alexdiasprojetos@gmail.com>
2020-05-02 11:39:40 -0300

    atualização 2

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-function definir() {
+function verificar() {
     let lado1 = document.getElementById("lado1").value
     let lado2 = document.getElementById("lado2").value
     let lado3 = document.getElementById("lado3").value
