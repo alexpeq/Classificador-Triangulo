c0ce32e5ac7b524856f0969cc737343d4e4b93e6
Alex Dias <alexdiasprojetos@gmail.com>
2020-05-02 11:37:05 -0300

    atualização 2

---
+++ script.js
@@ -0,0 +1,4 @@
+function definir() {
+    let lado1 = document.getElementById("lado1").value
+    let lado2 = document.getElementById("lado2").value
+    let lado3 = document.getElementById("lado3").value
